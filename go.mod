054485b343e7dffdfd64b5ec7e7082c06f7168b1
BaSO4 <wang73ying@163.com>
2022-04-12 22:49:06 +0800

    first commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module formula
 
 go 1.18
+
+require github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220411234842-3e3fecf10672
--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"formula/parser"
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+)
+
+type calcListener struct {
+	*parser.BaseCalcListener
+}
 
 func main() {
-	fmt.Println("hello antlr")
+	// Setup the input
+	is := antlr.NewInputStream("1 + 2 * 3")
+
+	// Create the Lexer
+	lexer := parser.NewCalcLexer(is)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	// Create the Parser
+	p := parser.NewCalcParser(stream)
+
+	// Finally parse the expression
+	antlr.ParseTreeWalkerDefault.Walk(&calcListener{}, p.Start())
+}
+
+func main1() {
+	// Setup the input
+	is := antlr.NewInputStream("1 + 2 * 3")
+
+	// Create the Lexer
+	lexer := parser.NewCalcLexer(is)
+
+	// Read all tokens
+	for {
+		t := lexer.NextToken()
+		if t.GetTokenType() == antlr.TokenEOF {
+			break
+		}
+		fmt.Printf("%s (%q)\n",
+			lexer.SymbolicNames[t.GetTokenType()], t.GetText())
+	}
 }
